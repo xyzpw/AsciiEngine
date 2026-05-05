33ab843780df540934159c985dac61fc01173859
xyzpw <76017734+xyzpw@users.noreply.github.com>
2026-05-05 02:18:31 -0400

    feat: add lerp, inverse lerp, and clamp01

--- include/AsciiEngine/utils/math.hpp
+++ include/AsciiEngine/utils/math.hpp
@@ -5,4 +5,30 @@
 namespace AsciiEngine::Utils
 {
 	bool boundsOverlap(const Math::Bounds &a, const Math::Bounds &b);
+
+	template<typename T, typename U>
+	constexpr T lerp(T a, T b, U t) noexcept
+	{
+		return a + t * (b - a);
+	}
+
+	template<typename T, typename U>
+	constexpr T inverseLerp(T a, T b, U value) noexcept
+	{
+		return (value - a) / (b - a);
+	}
+
+	/*
+	 * return: value clamped between 0 and 1
+	 */
+	template<typename T>
+	constexpr T clamp01(T value) noexcept
+	{
+		if (value < 0)
+			return 0;
+		if (value > 1)
+			return 1;
+
+		return value;
+	}
 }
