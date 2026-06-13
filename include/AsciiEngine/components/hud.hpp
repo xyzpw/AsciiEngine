0ba1e138d925081d4f129f5bd41d2f351dbe5220
xyzpw <76017734+xyzpw@users.noreply.github.com>
2026-06-13 11:38:53 -0400

    feat: add bold, blink, and color support for text

--- include/AsciiEngine/components/hud.hpp
+++ include/AsciiEngine/components/hud.hpp
@@ -2,6 +2,7 @@
 
 #include "AsciiEngine/core/component.hpp"
 #include "AsciiEngine/math/vector2.hpp"
+#include "AsciiEngine/common/color.hpp"
 #include <string>
 
 namespace AsciiEngine
@@ -9,6 +10,9 @@ namespace AsciiEngine
 	struct Hud : Component {
 		std::string text;
 		Math::Vector2 position;
+
+		bool blink = false;
+		bool bold = false;
 
 		Hud(const std::string &text) : text(text) {}
 		Hud(const std::string &text, const Math::Vector2 &point);
@@ -21,5 +25,20 @@ namespace AsciiEngine
 		 * Ncurses must be initialized before calling.
 		 */
 		void centerTextColumn();
+
+		/*
+		 * set color of text
+		 */
+		void setColor(Color fg, Color bg = Color::None);
+
+		void clearColor() { colorPairId = -1; }
+
+		Color getFgColor() const;
+		Color getBgColor() const;
+
+		private:
+		int colorPairId = -1;
+
+		void setAttrStatus(bool value);
 	};
 }
--- src/components/hud.cc
+++ src/components/hud.cc
@@ -1,6 +1,7 @@
 #include "AsciiEngine/components/hud.hpp"
 #include "AsciiEngine/math/vector2.hpp"
 #include "AsciiEngine/engine.hpp"
+#include "AsciiEngine/utils/colors.hpp"
 #include <string>
 #include <ncurses.h>
 
@@ -16,7 +17,9 @@ namespace AsciiEngine
 
 	void Hud::render()
 	{
+		setAttrStatus(true);
 		mvprintw(position.y, position.x, "%s", text.c_str());
+		setAttrStatus(false);
 	}
 
 	void Hud::centerTextColumn()
@@ -30,5 +33,44 @@ namespace AsciiEngine
 		int halfSize = 0.5f * text.size();
 
 		position.x = mid - halfSize;
+	}
+
+	void Hud::setColor(Color fgColor, Color bgColor)
+	{
+		colorPairId = Utils::getOrCreateColorPairId(fgColor, bgColor);
+	}
+
+	Color Hud::getFgColor() const
+	{
+		if (colorPairId == -1)
+			return Color::None;
+
+		return colorRegistry[colorPairId].fg;
+	}
+
+	Color Hud::getBgColor() const
+	{
+		if (colorPairId == -1)
+			return Color::None;
+
+		return colorRegistry[colorPairId].bg;
+	}
+
+	void Hud::setAttrStatus(bool value)
+	{
+		if (colorPairId != -1 && value)
+			attron(COLOR_PAIR(colorPairId));
+		else if (colorPairId != -1 && !value)
+			attroff(COLOR_PAIR(colorPairId));
+
+		if (bold && value)
+			attron(A_BOLD);
+		else if (bold && !value)
+			attroff(A_BOLD);
+
+		if (blink && value)
+			attron(A_BLINK);
+		else if (blink && !value)
+			attroff(A_BLINK);
 	}
 }
